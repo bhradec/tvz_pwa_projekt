25264dd9fecabe0f676678fba046cc3a6675f8b4
Bruno Hradec <bhradec@gmail.com>
2020-06-28 16:49:47 +0200

    dodan viewport

--- izmjena.php
+++ izmjena.php
@@ -68,6 +68,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>L'Express</title>
     <link rel="icon" type="image/png" href="resources/favicon.png"/>
     <link rel="stylesheet" href="style.css">
